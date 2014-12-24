80459927e2dfb8c14edafc4cdb2d18b4165a5bfa
Xiaming Chen <chenxm35@gmail.com>
2014-12-24 18:14:32 +0800

    First usable package

---
+++ R/omniR.R
@@ -0,0 +1,5 @@
+#' omniR: useful functions for spatio-temporal data analysis
+#'
+#' Package containing useful functions in my researches of human
+#' behavior science, mostly for spatio-temporal data
+NULL
