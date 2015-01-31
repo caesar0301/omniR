2398e7b8cd2acb28345a2c2f677a08762b9b571c
Xiaming Chen <chenxm35@gmail.com>
2015-01-31 11:05:10 +0800

    Add project settings

--- R/omniR.R
+++ R/omniR.R
@@ -1,5 +1,8 @@
 #' omniR: useful functions for spatio-temporal data analysis
 #'
 #' Package containing useful functions in my researches of human
-#' behavior science, mostly for spatio-temporal data
+#' behavior science, mostly for spatio-temporal data.
+#' 
+#' @docType package
+#' @name omniR
 NULL
